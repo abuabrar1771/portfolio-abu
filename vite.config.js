dd877110f00b9af632049aa3f7435da9a726e3e0
Mohamed Faizal <a.mohamedfaizal@gmail.com>
2025-12-24 22:25:12 +0530

    Fix: adjust build settings for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-   base: "/abu-portfolio/",
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
-});
+  base: './', // Add this line!
+})
