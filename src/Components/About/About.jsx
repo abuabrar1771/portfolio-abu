c75e1ff8d51b02b363dad424b246dd4540953c51
Mohamed Faizal <a.mohamedfaizal@gmail.com>
2025-12-24 23:16:09 +0530

    about page altered

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,15 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, forwardRef } from "react";
 import AboutCss from "./About.module.css";
 import About_img from "../../assets/abu-4.png";
 import Typed from "typed.js";
 
 import AOS from "aos";
-import "aos/dist/aos.css"; 
+import "aos/dist/aos.css";
 
 function About() {
   useEffect(() => {
     const typed = new Typed(".multiple", {
-      strings: ["Java Developer", "Android Developer", "iOS Developer", "Angular Developer"],
+      strings: [
+        "Java Developer",
+        "Android Developer",
+        "iOS Developer",
+        "Angular Developer",
+      ],
       typeSpeed: 80,
       backSpeed: 60,
       backDelay: 1300,
@@ -23,26 +28,46 @@ function About() {
     };
   }, []);
   const [isOpen, setIsOpen] = useState(false);
-    
-      useEffect(() => {
-        AOS.init({
-          duration: 1500,
-          easing: "ease-in-out",
-          once: true, // animation happens only once
-        });
-      }, []);
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1500,
+      easing: "ease-in-out",
+      once: true, // animation happens only once
+    });
+  }, []);
+  const MyButton = forwardRef((props, ref) => {
+    return (
+      <button ref={ref} {...props}>
+        Click Me
+      </button>
+    );
+  });
   return (
     <>
       <section id="about">
         <div className={AboutCss.about}>
-          <div className={AboutCss.about_img} data-aos="fade-right" data-aos-delay="200">
+          <div
+            className={AboutCss.about_img}
+            data-aos="fade-right"
+            data-aos-delay="200"
+          >
             <img src={About_img} alt="" />
           </div>
-          <div className={AboutCss.about_info} data-aos="fade-right" data-aos-delay="400" >
-            <h2 data-aos="fade-right" data-aos-dely="600">About Me</h2 >
-            <h3 data-aos="fade-right" data-aos-dely="800"> Abrar Ahamed</h3>
+          <div
+            className={AboutCss.about_info}
+            data-aos="fade-right"
+            data-aos-delay="400"
+          >
+            <h2 data-aos="fade-right" data-aos-dely="600">
+              About Me
+            </h2>
+            <h3 data-aos="fade-right" data-aos-dely="800">
+              {" "}
+              Abrar Ahamed
+            </h3>
             <h4 data-aos="fade-right" data-aos-dely="1000">
-              Hi I am&nbsp;<span className="multiple"></span> 
+              Hi I am&nbsp;<span className="multiple"></span>
             </h4>
             <p data-aos="fade-right" data-aos-delay="1200">
               I am a versatile developer specializing in Java, Android, iOS, and
@@ -52,7 +77,9 @@ function About() {
               maintainable code. I am passionate about learning new technologies
               and delivering reliable solutions across mobile and web platforms.
             </p>
-            <button>Download Cv</button>
+            <a href="/abu-resume.pdf" target="_blank" rel="noopener noreferrer">
+              <button className="download-btn">Download CV</button>
+            </a>
           </div>
         </div>
       </section>
